ccbc06c41db2a1d62e29415f5aec93284fb6523a
Fabian Lipp <fabian.lipp@uni-wuerzburg.de>
2020-06-10 23:53:11 +0200

    Initial version of ballot box

---
+++ ballot-box/app.py
@@ -0,0 +1,77 @@
+import json
+from typing import List
+
+from flask import Flask, g, request, render_template
+from .db import my_session_scope, MyDatabase, MyDatabaseSession, Poll, Vote
+
+app = Flask(__name__)
+
+my_database = MyDatabase('sqlite:///./testdb.sqlite')
+
+
+@app.route('/<poll_id>')
+def vote_form(poll_id):
+    with my_session_scope(my_database) as session:  # type: MyDatabaseSession
+        poll: Poll = session.get_poll_by_id(poll_id)
+        if not poll.active:
+            return render_template('message.html', state="not_active", poll_label=poll.label, poll_id=poll_id)
+        return render_template('index.html', poll_id=poll_id, poll_label=poll.label, answer_options=poll.answer_options)
+
+
+@app.route('/<poll_id>/submit_vote', methods=["POST"])
+def submit_vote(poll_id):
+    token = request.form['token']
+    answer = request.form['answer']
+    with my_session_scope(my_database) as session:  # type: MyDatabaseSession
+        poll: Poll = session.get_poll_by_id(poll_id)
+        vote: Vote = session.get_vote(poll_id, token)
+        if not vote:
+            return render_template('message.html', poll_label=poll.label, state="token_invalid")
+        if not poll.active:
+            return render_template('message.html', poll_label=poll.label, state="not_active")
+        vote.answer_id = answer
+        return render_template('message.html', poll_label=poll.label, state="successful")
+
+
+@app.route('/admin')
+@app.route('/admin/')
+def admin_overview():
+    with my_session_scope(my_database) as session:  # type: MyDatabaseSession
+        active_polls = session.get_polls(True)
+        closed_polls = session.get_polls(False)
+        return render_template('admin_overview.html', active_polls=active_polls, closed_polls=closed_polls)
+
+
+@app.route('/admin/new_poll', methods=['GET', 'POST'])
+def new_poll():
+    if request.method == 'GET':
+        return render_template('admin_new_poll.html')
+    elif request.method == 'POST':
+        label = request.form["label"]
+        request_answers: List[str] = request.form.getlist("answer[]")
+        answer_options = [x.strip() for x in request_answers if x.strip()]
+        request_tokens = json.loads(request.form["tokens"])
+        tokens = request_tokens['tokens']
+        with my_session_scope(my_database) as session:  # type: MyDatabaseSession
+            poll = session.add_poll(label, True, answer_options, tokens)
+            return render_template('admin_message.html', msg="create_success", poll_id = poll.poll_id)
+
+
+@app.route('/admin/close_poll/<poll_id>')
+def close_poll(poll_id):
+    with my_session_scope(my_database) as session:  # type: MyDatabaseSession
+        session.close_poll(poll_id)
+        return render_template('admin_message.html', msg="poll_closed", poll_id=poll_id)
+
+
+@app.route('/admin/poll_results/<poll_id>')
+def poll_results(poll_id):
+    with my_session_scope(my_database) as session:  # type: MyDatabaseSession
+        poll = session.get_poll_by_id(poll_id)
+        answer_options = session.get_results(poll_id)
+        votes = session.get_votes(poll_id)
+        return render_template('admin_poll_results.html', poll=poll, answer_options=answer_options, votes=votes)
+
+
+if __name__ == '__main__':
+    Flask.run(app, debug=True, host="0.0.0.0")
---
+++ ballot-box/db.py
@@ -0,0 +1,162 @@
+from contextlib import contextmanager
+from sqlite3 import Connection as SQLite3Connection
+from typing import List
+
+import sqlalchemy.engine
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, event, create_engine, func
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship, Session, Query
+from sqlalchemy.sql.expression import literal_column
+
+Base = declarative_base()
+
+
+class Poll(Base):
+    __tablename__ = "poll"
+    poll_id = Column(Integer, primary_key=True)
+    active = Column(Boolean, nullable=False, default=False)
+    label = Column(String(1024), nullable=False)
+
+    answer_options: List = relationship("AnswerOption")
+    votes: List = relationship("Vote")
+
+    #def __init__(self, chat_id, username, first_name, last_name, time_start=None):
+    #    self.chat_id = chat_id
+    #    self.username = username
+    #    self.first_name = first_name
+    #    self.last_name = last_name
+
+    #    if time_start is not None:
+    #        self.time_start = time_start
+    #    else:
+    #        self.time_start = int(time.time())
+    #    self.last_msg = None
+
+    #def __repr__(self):
+    #    return "<User(chat_id='%s', username='%s', first_name='%s', last_name='%s')>" \
+    #           % (self.chat_id, self.username, self.first_name, self.last_name)
+
+
+class AnswerOption(Base):
+    __tablename__ = "answer_option"
+    answer_id = Column(Integer, primary_key=True)
+    poll_id = Column(Integer, ForeignKey(Poll.poll_id), nullable=False)
+    label = Column(String(1024), nullable=False)
+
+    def __init__(self, label):
+        self.label = label
+
+
+class Vote(Base):
+    __tablename__ = "vote"
+    poll_id = Column(Integer, ForeignKey(Poll.poll_id), primary_key=True)
+    token = Column(String(20), primary_key=True)
+    answer_id = Column(Integer, ForeignKey(AnswerOption.answer_id), nullable=True)
+    answer_option = relationship("AnswerOption")
+
+    def __init__(self, token):
+        self.answer_id = None
+        self.token = token
+
+
+class MyDatabaseSession:
+    session = None
+
+    def __init__(self, session: Session):
+        self.session = session
+
+    def commit(self):
+        self.session.commit()
+
+    def close(self):
+        self.session.close()
+
+    def rollback(self):
+        self.session.rollback()
+
+    def get_poll_by_id(self, poll_id: int) -> Poll:
+        return self.session.query(Poll).filter(Poll.poll_id == poll_id).first()
+
+    def get_polls(self, active: bool) -> List[Poll]:
+        return self.session.query(Poll).filter(Poll.active == active).all()
+
+    def add_poll(self, label: str, active: bool, answer_options: List[str], tokens: List[str]) -> Poll:
+        poll = Poll()
+        poll.label = label
+        poll.active = active
+        for answer_label in answer_options:
+            poll.answer_options.append(AnswerOption(answer_label))
+        for token in tokens:
+            poll.votes.append(Vote(token))
+        self.session.add(poll)
+        self.session.flush()
+        return poll
+
+    def close_poll(self, poll_id: int):
+        poll: Poll = self.get_poll_by_id(poll_id)
+        poll.active = False
+
+    def get_vote(self, poll_id, token) -> Vote:
+        return self.session.query(Vote).filter(Vote.poll_id == poll_id, Vote.token == token).first()
+
+    def get_votes(self, poll_id) -> List[Vote]:
+        return self.session.query(Vote).filter(Vote.poll_id == poll_id).all()
+
+    def get_results(self, poll_id) -> List:
+        # This union of queries simulates a full outer join
+        q1: Query = self.session.query(Vote.answer_id, AnswerOption.label, func.count(Vote.token).label("count"))\
+            .join(AnswerOption, isouter=True)\
+            .filter(Vote.poll_id == poll_id).group_by(Vote.answer_id)
+        q2: Query = self.session.query(AnswerOption.answer_id, AnswerOption.label, literal_column("0").label("count"))\
+            .join(Vote, isouter=True)\
+            .filter(AnswerOption.poll_id == poll_id, Vote.token.is_(None))
+        return q1.union(q2).all()
+
+
+class MyDatabase:
+    db_engine = None
+
+    def __init__(self, database_url):
+        self.db_engine = create_engine(database_url, pool_pre_ping=True, echo=False)
+        try:
+            Base.metadata.create_all(self.db_engine)
+            # print("Tables created")
+        except Exception as e:
+            print("Error occurred during Table creation!")
+            print(e)
+        self.Session = sessionmaker(bind=self.db_engine)
+
+    def get_session(self) -> MyDatabaseSession:
+        return MyDatabaseSession(self.Session())
+
+
+@contextmanager
+def my_session_scope(db: MyDatabase):
+    """Provide a transactional scope around a series of operations."""
+    session = db.get_session()
+    try:
+        yield session
+        session.commit()
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
+
+
+@event.listens_for(sqlalchemy.engine.Engine, "connect")
+def _set_sqlite_pragma(dbapi_connection, _connection_record):
+    if isinstance(dbapi_connection, SQLite3Connection):
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON;")
+        cursor.close()
+
+
+def db_test(database_url):
+    db = MyDatabase(database_url)
+    with my_session_scope(db) as session:  # type: MyDatabaseSession
+        session.add_poll("test-poll", True, ["ja", "nein"], ["abc1", "abc2"])
+        session.add_poll("test-poll-beta", False, ["ja", "nein", "vielleicht"], ["def1", "def2", "def3"])
+
+
+# db_test('sqlite:///./testdb.sqlite')
